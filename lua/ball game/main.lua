e2a136f50dc7cfff83c53414bf8577db768c67dd
Medic314 <maxxmazola@gmail.com>
2025-10-27 08:09:44 -0400

    even more stuff

--- lua/ball game/main.lua
+++ lua/ball game/main.lua
@@ -1,11 +1,14 @@
 local love =  require("love")
+local enemy = require("enemy")
+
+math.randomseed(os.time())
 
 local game = {
     difficulty = 1,
     state = {
         menu = true,
         paused = false,
-        running = false,
+        running = true,
         ended = false
     }
 }
@@ -16,17 +19,21 @@ local player = {
     y = 30
 }
 
-local enemies ={
-    
-}
+local enemies = {}
 
 function love.load()
     love.window.setTitle("Save the Ball")
     love.mouse.setVisible(false)
+
+    table.insert(enemies, 1, Enemy())
 end
 
 function love.update()
     player.x, player.y = love.mouse.getPosition()
+
+    for i = 1, #enemies do
+        enemies[i]:move(player.x, player.y)
+    end
 end
 
 function love.draw()
@@ -34,6 +41,10 @@ function love.draw()
     love.graphics.printf("FPS: " .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
 
     if game.state["running"] then
+        for i = 1, #enemies do
+        enemies[i]:draw()
+    end
+
         love.graphics.circle("fill", player.x, player.y, player.radius)
     end
     
